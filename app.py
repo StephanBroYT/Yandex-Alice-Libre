9c922c98ee7357a25c54ffa0192a30d5b1977cef
StephanBro_YT <StephanBroYT@users.noreply.github.com>
2024-10-05 11:06:37 +0300

    Фиксы
    Фикс предупреждение о высоком/низком уровне глюкозы

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ logging.basicConfig(level=logging.DEBUG)
 username = "your libre link up email"               # ВСТАВЬТЕ СВОИ ДАННЫЕ
 password = "your libre link up password"
 port = 5000                                         # ПОРТ
+max_range = 10                                      # Предупреждение
+min_range = 3.9
 
 url = "https://api.libreview.ru"                    # API в России
 version = "4.2.1"
@@ -58,8 +60,6 @@ def main():
 
         curret = connections["data"][0]["glucoseMeasurement"]["Value"]
         arrow = connections["data"][0]["glucoseMeasurement"]["TrendArrow"]
-        ishigh = connections["data"][0]["glucoseMeasurement"]["isHigh"]
-        islow = connections["data"][0]["glucoseMeasurement"]["isLow"]
 
         arrow_text = "ошибка"
         status = ""
@@ -75,10 +75,16 @@ def main():
         elif arrow == 1:
             arrow_text = "вниз"
 
-        if ishigh:
+        # if ishigh:                                                   Не работает
+        #     status = "Осторожно, высокий уровень глюкозы, "
+        # elif islow:
+        #     status = "Осторожно, низкий уровень глюкозы, "
+        
+        if curret >= max_range:
             status = "Осторожно, высокий уровень глюкозы, "
-        elif islow:
+        elif curret <= min_range:
             status = "Осторожно, низкий уровень глюкозы, "
+            
 
         measurement_time = datetime.strptime(connections["data"][0]["glucoseMeasurement"]["Timestamp"], "%m/%d/%Y %I:%M:%S %p")
         local_tz = pytz.timezone('Europe/Moscow') 
@@ -94,45 +100,6 @@ def main():
         
     else:
         pass
-        # response = requests.get(f"{url}/llu/connections", headers=headers)
-        # connections = response.json()
-        # print(connections["data"][0]["glucoseMeasurement"])
-
-        # curret = connections["data"][0]["glucoseMeasurement"]["Value"]
-        # arrow = connections["data"][0]["glucoseMeasurement"]["TrendArrow"]
-        # ishigh = connections["data"][0]["glucoseMeasurement"]["isHigh"]
-        # islow = connections["data"][0]["glucoseMeasurement"]["isLow"]
-
-        # arrow_text = "ошибка"
-        # status = ""
-
-        # if arrow == 3:
-        #     arrow_text = "стабильная"
-        # elif arrow == 4:
-        #     arrow_text = "диагональная вверх"
-        # elif arrow == 5:
-        #     arrow_text = "вверх"
-        # elif arrow == 2:
-        #     arrow_text = "диагональная вниз"
-        # elif arrow == 1:
-        #     arrow_text = "вниз"
-
-        # if ishigh:
-        #     status = "Осторожно, высокий уровень глюкозы, "
-        # elif islow:
-        #     status = "Осторожно, низкий уровень глюкозы, "
-
-        # measurement_time = datetime.strptime(connections["data"][0]["glucoseMeasurement"]["Timestamp"], "%m/%d/%Y %I:%M:%S %p")
-        # local_tz = pytz.timezone('Europe/Moscow') 
-        # measurement_time = local_tz.localize(measurement_time)
-        # now = datetime.now(local_tz)
-        # minutes_ago = (now - measurement_time).total_seconds() // 60
-
-        # at = measurement_time.strftime('%H:%M')
-        # ago = f"{int(minutes_ago)} минут назад" if minutes_ago < 60 else f"{int(minutes_ago // 60)} часов {int(minutes_ago % 60)} минут назад"
-
-        # rs = f"{status}Текущий уровень глюкозы {curret}, был отсканирован в {at}, это {ago}, стрелка {arrow_text}"
-        # response1["response"]["text"] = rs
 
     return json.dumps(response1)
 
